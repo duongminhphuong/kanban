ce4d8f9854a3d552b7a4b3fee28ad3704cc3adf2
Hao Pham <haophamluongg@gmail.com>
2019-10-12 15:31:59 +0700

    add table in home page

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-          <Button variant="success" className="add_button">Add new column</Button>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: []
+    }
+  }
+  add_new_column(column) {
+    this.setState({
+      list: [...this.state.list, column]
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <table>
+            <tr>
+                {this.state.list.map((item, index) => (<td>{item.name}</td>))}
+            </tr>
+
+          </table>
+        </div>
+        <p>
+          <Button variant="success" className="add_button" onClick={this.add_new_column.bind(this, { name: "OK", items: [] })}>Add new column</Button>
         </p>
-      {/* <header className="App-header">
-        
-      </header> */}
-      
-    </div>
-  );
+
+      </div>
+    );
+  }
 }
 
 export default App;
