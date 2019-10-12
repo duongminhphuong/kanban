a684795122a6eac0b72c587ba2b272547e72ce1a
Hao Pham <haophamluongg@gmail.com>
2019-10-12 16:41:53 +0700

    add insert and delete function

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import TrelloList from './components/TrelloList'
 import { Button } from 'react-bootstrap';
-
+//172.28.165.156:5000
 
 class App extends React.Component {
   constructor(props) {
@@ -17,45 +17,39 @@ class App extends React.Component {
       list: [...this.state.list, column]
     })
   }
+
+  delete_column(e) {
+    console.log(e.target.value)
+    this.setState({
+      list: this.state.list.filter((x,i) => i != e.target.value)
+    })
+  }
   render() {
     return (
       <div className="App">
         <div>
+          <div>
+            <Button variant="success" className="add_button" onClick={this.add_new_column.bind(this, { name: "OK", items: [] })}>Add new column</Button>
+          </div>
+          <div>
           <table>
             <tr>
-                {this.state.list.map((item, index) => (<td>{item.name}</td>))}
+                {this.state.list.map((item, index) => (<td>{<TrelloList title="DER TITLE!" ></TrelloList>}</td>))}
             </tr>
-
+            <tr>
+                {this.state.list.map((item, index) => (<td>
+                  <Button variant="warning" value={index} onClick={e => this.delete_column(e, "value")}>
+                    X
+                  </Button>
+                </td>))}
+            </tr>
           </table>
+          </div>
         </div>
-        <p>
-          <Button variant="success" className="add_button" onClick={this.add_new_column.bind(this, { name: "OK", items: [] })}>Add new column</Button>
-        </p>
+        
 
       </div>
     );
   }
-import TrelloList from './components/List'
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <TrelloList title="DER TITLE!" ></TrelloList>
-  );
 }
-
 export default App;
